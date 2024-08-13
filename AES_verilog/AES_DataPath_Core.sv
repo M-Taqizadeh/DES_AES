4db1badca78d80bb3c1d651f48300f09b7d11279
M-Taqizadeh <mehrdadtaqizadehfazly@gmail.com>
2024-08-13 14:32:39 +0330

    mix column decipher mode

--- AES_verilog/AES_DataPath_Core.sv
+++ AES_verilog/AES_DataPath_Core.sv
@@ -13,8 +13,6 @@ module AES_DataPath_Core (
     // output
     output reg [7:0] data_out
 );
-    wire cipher;
-    assign cipher = ~decipher;
     wire [7:0] parallel_d_in0;
     wire [7:0] parallel_d_in1;
     wire [7:0] parallel_d_in2;
@@ -34,7 +32,7 @@ module AES_DataPath_Core (
     wire [7:0] BPU_in_data;
     assign BPU_in_data = d_out_parallel_to_serial ^ round_key_delayed;
     wire [7:0] BPU_out_data;
-    assign shift_left = ~decipher;
+    assign shift_left = cipher;
     Byte_Permutation_Unit BPU(
         .clk(clk),
         .rst(rst),
@@ -53,6 +51,7 @@ module AES_DataPath_Core (
         .rst(rst),
         .clk(clk),
         .en(en),
+        .encrypt(cipher)
         .d_in(sbox_out),
         .d_out0(parallel_d_in0),
         .d_out1(parallel_d_in1),
--- AES_verilog/Mix_Column.sv
+++ AES_verilog/Mix_Column.sv
@@ -2,20 +2,46 @@ module Mix_Column (
     input wire rst,
     input wire clk,
     input wire en,
+    input wire encrypt,
     input wire [7:0] d_in,
     output reg [7:0] d_out0,
     output reg [7:0] d_out1,
     output reg [7:0] d_out2,
     output reg [7:0] d_out3,
 );
-    wire [7:0] d_in_tiems2, d_in_tiems3;
+    wire [7:0] d_in_times2
+    wire [7:0] d_in_times3;
+    wire [7:0] d_in_times4;
+    wire [7:0] d_in_times5;
+    wire [7:0] d_in_times6;
+    wire [7:0] d_in_times7;
+    wire [7:0] d_in_times8;
+    wire [7:0] d_in_times9;
+    wire [7:0] d_in_times10;
+    wire [7:0] d_in_times11;
+    wire [7:0] d_in_times12;
+    wire [7:0] d_in_times13;
+    wire [7:0] d_in_times14;
     wire [7:0] r0_in, r1_in, r2_in, r3_in;
-    assign d_in_tiems2 = d_in[7] ? d_in << 1 : (d_in << 1) ^ 8'h1b;
-    assign d_in_tiems3 = d_in_tiems2 ^ d_in;
-    assign r0_in = d_in ^ (en & d_out1)
-    assign r1_in = d_in ^ (en & d_out2)
-    assign r2_in = d_in_tiems3 ^ (en & d_out3)
-    assign r3_in = d_in_tiems2 ^ (en & d_out0)
+    
+    assign d_in_times2 = d_in[7] ? d_in << 1 : (d_in << 1) ^ 8'h1b;
+    assign d_in_times3 = d_in_times2 ^ d_in;
+    assign d_in_times4 = d_in_times2[7] ? d_in_times2 << 1 : (d_in_times2 << 1) ^ 8'h1b;
+    assign d_in_times5 = d_in_times4 ^ d_in;
+    assign d_in_times6 = d_in_times3[7] ? d_in_times3 << 1 : (d_in_times3 << 1) ^ 8'h1b;
+    assign d_in_times7 = d_in_times6 ^ d_in;
+    assign d_in_times8 = d_in_times4[7] ? d_in_times4 << 1 : (d_in_times4 << 1) ^ 8'h1b;
+    assign d_in_times9 = d_in_times_8 ^ d_in;
+    assign d_in_times10 = d_in_times5[7] ? d_in_times5 << 1 : (d_in_times5 << 1) ^ 8'h1b;
+    assign d_in_times11 = d_in_times_10 ^ d_in;
+    assign d_in_times12 = d_in_times6[7] ? d_in_times6 << 1 : (d_in_times6 << 1) ^ 8'h1b;
+    assign d_in_times13 = d_in_times_12 ^ d_in;
+    assign d_in_times14 = d_in_times7[7] ? d_in_times7 << 1 : (d_in_times7 << 1) ^ 8'h1b;
+
+    assign r0_in = (encrypt ? d_in : d_in_times9) ^ (en & d_out1)
+    assign r1_in = (encrypt ? d_in : d_in_times13) ^ (en & d_out2)
+    assign r2_in = (encrypt ? d_in_times3 : d_in_times11) ^ (en & d_out3)
+    assign r3_in = (encrypt ? d_in_times2 : d_in_times14) ^ (en & d_out0)
     always @(posedge clk or posedge rst) begin
         if(rst)begin
             d_out0 <= 8'b0;
